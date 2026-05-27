efd327da400336e18f2b19f61e6ea777391e70f3
Rajvardhan <rajvardhan.ganji75@gmail.com>
2026-05-27 22:33:01 +0530

    Initial Commit

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting Broker Service on Port %s\n", webPort)
+
+	// define Http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// Start the Server
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Panic(err)
+	}
+}
